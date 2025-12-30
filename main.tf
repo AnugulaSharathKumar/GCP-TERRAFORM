fdbd645b87ba5abb67567744ea765df91154a074
AnugulaSharathKumar <91570664+AnugulaSharathKumar@users.noreply.github.com>
2025-12-30 20:55:51 +0530

    Set up Google provider and module calls in main.tf
    Configured Google Cloud provider and added VPC and VM module calls with necessary input variables.

--- main.tf
+++ main.tf
@@ -1,1 +1,44 @@
+########################################
+# PROVIDER CONFIGURATION
+########################################
+provider "google" {
+  # GCP project ID where resources will be created
+  project = var.project_id
+
+  # Default region for resources
+  region  = var.region
+
+  # Default zone for Compute Engine
+  zone    = var.zone
+}
+
+########################################
+# VPC MODULE CALL
+########################################
+module "vpc" {
+  # Path to the VPC module
+  source = "./modules/vpc"
+
+  # Input variables passed to the VPC module
+  vpc_name    = var.vpc_name
+  subnet_name = var.subnet_name
+  subnet_cidr = var.subnet_cidr
+  region      = var.region
+}
+
+########################################
+# VM MODULE CALL
+########################################
+module "vm" {
+  # Path to the VM module
+  source = "./modules/vm"
+
+  # VM configuration
+  vm_name       = var.vm_name
+  machine_type = var.machine_type
+  zone          = var.zone
+
+  # Subnet ID received from VPC module output
+  subnet_id = module.vpc.subnet_id
+}
 
