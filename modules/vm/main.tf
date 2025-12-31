584921633dbd813c3608a012eb1c7d02b60ca442
AnugulaSharathKumar <91570664+AnugulaSharathKumar@users.noreply.github.com>
2025-12-31 09:30:27 +0000

    changed the disk size 20 to 10 GB

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -12,7 +12,7 @@ resource "google_compute_instance" "vm" {
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-2204-lts"
-      size  = 5  # disk size in GB
+      size  = 10 # disk size in GB
     }
   }
 
