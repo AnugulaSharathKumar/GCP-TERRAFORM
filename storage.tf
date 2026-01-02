87ea8011351efa607f8f9addc177a39827bfdd43
AnugulaSharathKumar <91570664+AnugulaSharathKumar@users.noreply.github.com>
2026-01-02 10:36:32 +0000

    Change the script

--- storage.tf
+++ storage.tf
@@ -1,17 +1,17 @@
-#resource "google_storage_bucket" "bucket"  {
-#  name          = var.bucket_name
-#  location      = var.bucket_location
-#  storage_class = var.storage_class
+resource "google_storage_bucket" "bucket"  {
+  name          = var.bucket_name
+  location      = var.bucket_location
+  storage_class = var.storage_class
 
-#  uniform_bucket_level_access = true
+  uniform_bucket_level_access = true
 
-#  lifecycle_rule {
-#    action {
-#      type = "Delete"
-#    }
-#    condition {
-#      age = 30
-#    }
-#  }
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30
+    }
+  }
   
-#}
+}
