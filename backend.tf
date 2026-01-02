86d96c259a223312760787bb2254853f2102d6f8
AnugulaSharathKumar <91570664+AnugulaSharathKumar@users.noreply.github.com>
2026-01-02 11:00:21 +0000

    Change the script

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
-#terraform {
-#  backend "gcs" {
-#    bucket  = "sharath-terraform-gcs-bucket-001"
-#    prefix  = "/my-learning-terraform-482905/Statefile/terraform-state"
-#  }
-#}
+terraform {
+  backend "gcs" {
+    bucket  = "sharath-terraform-gcs-bucket-001"
+    prefix  = "/my-learning-terraform-482905/Statefile/terraform-state"
+  }
+}
